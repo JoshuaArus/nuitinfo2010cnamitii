b48be0e3c832a32bfb6a1a4afb208be760511d3b
deveaux.cyril <deveaux.cyril@0a582a40-e9a4-69f9-b000-cbd835332137>
2010-12-02 22:43:07 +0000

    Script SQL OK

---
+++ trunk/BDD/5-nuitInfo_universite.sql
@@ -0,0 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.3
+-- http://www.phpmyadmin.net
+--
+-- Serveur: localhost
+-- Généré le : Jeu 02 Décembre 2010 à 22:36
+-- Version du serveur: 5.1.32
+-- Version de PHP: 5.2.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Base de données: `ortsfran`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nuitInfo_universite`
+--
+
+CREATE TABLE IF NOT EXISTS `nuitInfo_universite` (
+  `n_universite` bigint(20) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `n_adresse` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `nuitInfo_universite`
+--
+
---
+++ trunk/BDD/9-nuitInfo_trajet.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.3
+-- http://www.phpmyadmin.net
+--
+-- Serveur: localhost
+-- Généré le : Jeu 02 Décembre 2010 à 22:34
+-- Version du serveur: 5.1.32
+-- Version de PHP: 5.2.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Base de données: `ortsfran`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nuitInfo_trajet`
+--
+
+CREATE TABLE IF NOT EXISTS `nuitInfo_trajet` (
+  `n_trajet` bigint(20) NOT NULL,
+  `n_adresseDepart` bigint(20) NOT NULL,
+  `n_adresseArrive` bigint(20) NOT NULL,
+  `nbPlaceDisponible` int(11) NOT NULL,
+  `allerRetour` tinyint(1) NOT NULL,
+  `n_dateArrive` bigint(20) NOT NULL,
+  `n_dateDepart` bigint(20) NOT NULL,
+  `n_statutTrajet` bigint(20) NOT NULL,
+  PRIMARY KEY (`n_trajet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `nuitInfo_trajet`
+--
+
